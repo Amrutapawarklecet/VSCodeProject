d95fa6dbd2e7cb0b14f1fb8e4a251c4a4a2fd268
Amruta Pawar <amrutapawarklecet@gmail.com>
2024-04-03 09:39:28 +0530

    first commit

---
+++ JavaApp.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class JavaApp{
+    public static void main(String[] args){
+    System.out.println("Java Project");
+    System.out.println("Java Project out is");
+    System.out.println(new Date());
+    }
+}
