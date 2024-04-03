c5f9ff482df131adf2272566943432e3c2545bd4
Amruta Pawar <amrutapawarklecet@gmail.com>
2024-04-03 10:07:31 +0530

    error change

--- JavaApp.java
+++ JavaApp.java
@@ -5,7 +5,7 @@ public class JavaApp{
     System.out.println("Java Project");
     System.out.println("Java Project out is");
     System.out.println(new Date());
-    Student s=new Student();
+    Student s=new Student(1);
     s.sayHello();
     }
 }
